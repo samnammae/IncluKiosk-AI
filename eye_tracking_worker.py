d060e1c7e7cbe5e7b5969db95ee19175e4a72e98
kangeunsong <dmxthd@naver.com>
2025-10-13 01:27:15 +0900

    refactor: adjust value related to face

--- eye_tracking_worker.py
+++ eye_tracking_worker.py
@@ -256,8 +256,8 @@ face_mesh = mp_face_mesh.FaceMesh(
     static_image_mode=False,
     max_num_faces=1,
     refine_landmarks=True,
-    min_detection_confidence=0.4,
-    min_tracking_confidence=0.4
+    min_detection_confidence=0.3,
+    min_tracking_confidence=0.3
 )
 
 # MediaPipe Hands
