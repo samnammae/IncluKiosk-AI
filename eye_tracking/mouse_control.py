4599486f96dc3c78a478cea64bc669a15a263bad
kangeunsong <dmxthd@naver.com>
2025-10-17 18:58:57 +0900

    debug: add mouse running debug

--- eye_tracking/mouse_control.py
+++ eye_tracking/mouse_control.py
@@ -58,6 +58,7 @@ class MouseController:
         last_xy = (0, 0)
         
         while self.running:
+            print("🟠[Mouse] running mouse over thread")
             # 터치 중이거나 홀드오프 기간이면 대기
             if self.touch_active or (time.time() - self.last_touch_end < config.TOUCH_HOLDOFF):
                 time.sleep(0.01)
